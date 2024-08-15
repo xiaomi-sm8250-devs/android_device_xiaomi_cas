ccdf334c2e54c1f6ce7297c682dc6acdf14f26b6
Nanhumly <admin@warbler.cc>
2024-08-15 03:25:34 +0000

    cas: Set correct product model
    Change-Id: I59f8c19e93a59492230c7e8cce7a6430d9150f39

--- libinit/init_xiaomi_cas.cpp
+++ libinit/init_xiaomi_cas.cpp
@@ -15,8 +15,8 @@ static const variant_info_t cas_info = {
 
     .brand = "Xiaomi",
     .device = "cas",
-    .marketname = "",
-    .model = "Mi 10 Ultra",
+    .marketname = "Mi 10 Ultra",
+    .model = "M2007J1SC",
     .build_fingerprint = "Xiaomi/cas/cas:13/RKQ1.211001.001/V816.0.4.0.TJJCNXM:user/release-keys",
 
     .nfc = true,
